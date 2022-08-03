a7ee68f6614b9ed55f2b9d84220fc51d015ed0ad
Tifa06 <latifafahmi775@gmail.com>
2022-08-03 14:15:19 +0700

    komentar

---
+++ latihan.php
@@ -0,0 +1,14 @@
+<?php
+
+class mobil 
+{
+    public $merk;
+    public $tipe;
+    public $tahun;
+    public $bulan;
+    public $warna ='biru';
+}
+    $mobil = newMobil();
+    var_dump($mobil);
+
+?>
